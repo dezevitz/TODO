a546a819aae53c28201f3eb48d7743bcfdd1790c
Danielle Zevitz <daniellezevitz@Intern-CVille-12.local>
2021-06-08 16:51:48 -0400

    todoAdapter in progress

--- TodoApplication/app/src/main/java/com/example/todoapplication/TodoAdapter.kt
+++ TodoApplication/app/src/main/java/com/example/todoapplication/TodoAdapter.kt
@@ -1,10 +1,27 @@
 package com.example.todoapplication
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter (
     var todos: List<Todo>
-        ){
+        ): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>(){
     inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
+        return TodoViewHolder(view)
+    }
+    /*
+    * Holder used to access the view holder
+    * */
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
 }
